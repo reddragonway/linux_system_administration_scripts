b33b06377d1cfe1753c4091ea99a16776e2089fe
Vladimir [Nez]nanoFF <dark_strannik@mail.ru>
2021-01-26 23:31:37 +0300

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -5,5 +5,3 @@ echo "---------------------------------------------------------------"
 echo "Operating system : $(uname)"
 lsb_release -a 2>/dev/null || echo "lsb_release command not found"
 echo "Kernel : $(uname -r)"
-
-
